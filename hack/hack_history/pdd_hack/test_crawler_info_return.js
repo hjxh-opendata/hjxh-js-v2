cdd58801a8b1bbc573729777992d718cde9cd718
MarkShawn2020 <877210964@qq.com>
2021-05-06 11:50:00 +0800

    rm"

--- hack/hack_history/pdd_hack/test_crawler_info_return.js
+++
@@ -1,3 +0,0 @@
-const getCrawlerInfo = require("./crawler_info.js");
-
-console.log(getCrawlerInfo());
--- hack/hack_history/pdd_hack_5-convert_2/parse_t_n_cmd.js
+++
@@ -1,5 +0,0 @@
-const funcConvert = require("./parse_t_n");
-console.log({args: process.argv})
-const [a, b] = process.argv.slice(2, 4)
-
-console.log({a, b, res: funcConvert(a, b)})
